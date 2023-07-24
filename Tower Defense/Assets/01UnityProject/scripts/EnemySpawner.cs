53dc9c3fc42b90a92330dc2fade7443f2279e4b7
ZEZE\a0104 <a01049469494@gmail.com>
2023-07-24 18:00:29 +0900

    feat : 웨이브 시스템 추가

--- Tower Defense/Assets/01UnityProject/scripts/EnemySpawner.cs
+++ Tower Defense/Assets/01UnityProject/scripts/EnemySpawner.cs
@@ -4,14 +4,10 @@ using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
-    //[SerializeField]
-    //private GameObject enemyPrefab;                   // 적 프리팹
     [SerializeField]
     private GameObject enemyHPSliderPrefab;          // 적 체력을 나타내는 Slider UI 프리팹
     [SerializeField]
     private Transform canvasTransform;               // UI를 표현하는 Canvas 오브젝트의 Transform
-    //[SerializeField]
-    //private float spawnTime;                         // 적 생성 주기
     [SerializeField]
     private Transform[] wayPoints;                   // 현재 스테이지의 이동 경로
     [SerializeField]
@@ -19,32 +15,58 @@ public class EnemySpawner : MonoBehaviour
     [SerializeField]
     private PlayerGold playerGold;                   // 플레이어 골드 컴포넌트
     private Wave currentWave;                        // 현재 웨이브 정보
+    private int currentEnemyCount;                   // 현재 웨이브에 남아있는 적 숫자 (웨이브 시작시 max로 설정, 적 사망 시 -1)
     private List<Enemy> enemyList;                   // 현재 맵에 존재하는 모든 적의 정보
 
     // 적의 생성과 삭제는 Enemyspawner 에서 하기 때문에 set은 필요 없다.
     public List<Enemy> EnemyList => enemyList;
+    // 현재 웨이브에 남아있는적, 최대 적 숫자
+    public int CurrentEnemyCount => currentEnemyCount;
+    public int MaxEnemyCount => currentWave.maxEnemyCount;
 
     private void Awake()
     {
         //적 리스트 메모리 할당
         enemyList = new List<Enemy>();
-        //적 생성 코루틴 함수 호출
-        //StartCoroutine("SpawnEnemy");
+    }
+
+    public void StartWave(Wave wave)
+    {
+        // 매개변수로 받아온 웨이브 정보 저장
+        currentWave = wave;
+        // 현재 웨이브의 최대 적 숫자를 저장
+        currentEnemyCount = currentWave.maxEnemyCount;
+        // 현재 웨이브 시작
+        StartCoroutine("SpawnEnemy");
     }
 
     private IEnumerator SpawnEnemy()
     {
-        while(true)
-        {
-            GameObject clone = Instantiate(enemyPrefab);  // 적 오브젝트 생성
-            Enemy enemy = clone.GetComponent<Enemy>();    // 방금 생성된 적의 enemy 컴포넌트
+        ///현재 웨이브에서 생성한 적 숫자
+        int spawnEnemyCount = 0;
 
+        //while(true)
+        // 현재 웨이브에서 생성되어야 하는 적의 숫자만큼 적을 생성하고 코루틴 종료
+        while(spawnEnemyCount < currentWave.maxEnemyCount)
+        {
+            //GameObject clone = Instantiate(enemyPrefab);  // 적 오브젝트 생성
+            // 웨이브에 등장하는 적의 종류가 여러 종류일 때 임의의 적이 등장하도록 설정하고, 적 오브젝트 생성
+            int enemyIndex = Random.Range(0, currentWave.enemyPrefab.Length);
+            GameObject clone = Instantiate(currentWave.enemyPrefab[enemyIndex]);
+;            Enemy enemy = clone.GetComponent<Enemy>();    // 방금 생성된 적의 enemy 컴포넌트
+
+            // this는 나 자신 (자신의 EnemySpawner 정보)
             enemy.Setup(this, wayPoints);                 // waypoint  정보를 매개변수로 setup() 호출
             enemyList.Add(enemy);                         // 리스트에 방금 생성된 적 정보 저장
 
             SpawnEnemyHPSlider(clone);                    // 적 체력을 나타내는 Slider UI 생성 및 설정
 
-            yield return new WaitForSeconds(spawnTime);   // spawnTime 시간 동안 대기
+            // 현재 웨이브에서 생성한 적의 숫자 +1
+            spawnEnemyCount++;
+
+            //yield return new WaitForSeconds(spawnTime);   // spawnTime 시간 동안 대기
+            // 각 웨이브마다 spawnTime이 다를 수 있기 때문에 현재 웨이브(currentWave)의 spawnTime 사용
+            yield return new WaitForSeconds(currentWave.spawnTime);   // spawnTime 동안 대기
         }
     }
     
@@ -63,6 +85,8 @@ public class EnemySpawner : MonoBehaviour
             playerGold.CurrentGold += gold;
         }
 
+        // 적이 사망할 때마다 현재 웨이브의 생존 적 숫자 감소 (UI 표시용)
+        currentEnemyCount--;
         //리스트에서 사망하는 적 정보 삭제
         enemyList.Remove(enemy);
         //적 오브젝트 삭제
--- Tower Defense/Assets/01UnityProject/scripts/WaveSystem.cs
+++ Tower Defense/Assets/01UnityProject/scripts/WaveSystem.cs
@@ -4,7 +4,27 @@ using UnityEngine;
 
 public class WaveSystem : MonoBehaviour
 {
-    
+    [SerializeField]
+    private Wave[] waves;                                      // 현재 스테이지의 모든 웨이브 정보
+    [SerializeField]
+    private EnemySpawner enemySpawner;
+    private int currentWaveIndex = -1;                         // 현재 웨이브 인덱스
+
+    //웨이브 정보 출력을 위한 Get 프로퍼티 (현재 웨이브, 총 웨이브)
+    public int CurrentWave => currentWaveIndex + 1;            // 시작이 0이기 때문에 +1
+    public int MAxWave => waves.Length;
+
+    public void StartWave()
+    {
+        // 현재 맵에 적이 없고, wave가 남아있으면
+        if(enemySpawner.EnemyList.Count == 0 && currentWaveIndex < waves.Length -1)
+        {
+            // 인덱스의 시작이 -1 이기 떄문에 웨이브 인덱스 증가를 제일 먼저 함
+            currentWaveIndex ++;
+            // EnemySpawner의  StartWave() 함수 호출. 현재 웨이브 정보 제공
+            enemySpawner.StartWave(waves[currentWaveIndex]);
+        }
+    }
 }
 
 // 직렬화 하면 Inspector View 에서 클래스 내부의 변수 정보들을 수정할 수 있다.
