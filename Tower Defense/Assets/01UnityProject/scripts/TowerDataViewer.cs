aa0675bb94a3713dcc668e2a0d44a25dc1fac5af
ZEZE\a0104 <a01049469494@gmail.com>
2023-07-25 12:00:49 +0900

    feat : 타워 업그래이드 추가

--- Tower Defense/Assets/01UnityProject/scripts/TowerDataViewer.cs
+++ Tower Defense/Assets/01UnityProject/scripts/TowerDataViewer.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using JetBrains.Annotations;
 
 public class TowerDataViewer : MonoBehaviour
 {
@@ -18,6 +19,8 @@ public class TowerDataViewer : MonoBehaviour
     private TextMeshProUGUI textLevel;
     [SerializeField]
     private TowerAttackRange towerAttackRange;
+    [SerializeField]
+    private Button buttonUpgrade;  // 타워 업글이 최대가 되면 버튼 비활성화
 
     private TowerWeapon currentTower;
 
@@ -57,9 +60,31 @@ public class TowerDataViewer : MonoBehaviour
 
     private void UpdateTowerData()
     {
+        imageTower.sprite = currentTower.TowerSprite;
         textDamage.text = "Damage : " + currentTower.Damage;
         textRate.text = "Rate : " + currentTower.Rate;
         textRange.text = "Range : " + currentTower.Range;
         textLevel.text = "Level : " + currentTower.Level;
-    }    
+
+        // 업그레이드가 불가능해지면 버튼 비활성화
+        buttonUpgrade.interactable = currentTower.Level < currentTower.MaxLevel ? true : false;
+    }   
+    
+    public void OnClickEventTowerUpgrade()
+    {
+        // 타워 업ㄱ레이드 시도 (성공:true, 실패:false)
+        bool isSuccess = currentTower.Upgrade();
+
+        if(isSuccess == true)
+        {
+            // 타워가 업그레이드 되었기 때문에 타워 정보 갱신
+            UpdateTowerData();
+            // 타워 주변에 보이는 공격범위도 갱신
+            towerAttackRange.OnAttackRange(currentTower.transform.position, currentTower.Range);
+        }
+        else
+        {
+            // 타워 업글에 필요한 비용이 부족하다고 출력
+        }
+    }
 }
--- Tower Defense/Assets/01UnityProject/scripts/TowerWeapon.cs
+++ Tower Defense/Assets/01UnityProject/scripts/TowerWeapon.cs
@@ -8,28 +8,31 @@ public enum WeaponState { SearchTarget = 0, AttackToTarget }
 public class TowerWeapon : MonoBehaviour
 {
     [SerializeField]
+    private TowerTemplate towerTemplate;                        // 타워 정보 (공격력, 공격속도 등 )
+    [SerializeField]
     private GameObject projectilePrefab;                        // 발사체 프리팹
     [SerializeField]
     private Transform spawnPoint;                                // 발사체 생성 위치
-    [SerializeField]
-    private float attackRate = 0.5f;                             // 공격 속도
-    [SerializeField]
-    private float attackRange = 2.0f;                            // 공격 범위
-    [SerializeField]
-    private int attackDamage = 1;                                // 공격력
+   
     private int level = 0;                                       // 타워 레벨
     private WeaponState weaponState = WeaponState.SearchTarget;  // 타워 무기의 상태
     private Transform attackTarget = null;                       // 공격 대상
+    private SpriteRenderer spriteRenderer;                       // 타워 오브젝트 이미지 변경용
     private EnemySpawner enemySpawner;                           // 게임에 존재하는 적 정보 흭득용
+    private PlayerGold playerGold;                               // 플레이어의 골드 정보 흭득 및 설정
 
-    public float Damage => attackDamage;
-    public float Rate => attackRate;
-    public float Range => attackRange;
+    public Sprite TowerSprite => towerTemplate.weapon[level].sprite;
+    public float Damage => towerTemplate.weapon[level].damage;
+    public float Rate => towerTemplate.weapon[level].rate;
+    public float Range => towerTemplate.weapon[level].range;
     public int Level => level + 1;
+    public int MaxLevel => towerTemplate.weapon.Length;
 
-    public void Setup(EnemySpawner enemySpawner)
+    public void Setup(EnemySpawner enemySpawner, PlayerGold playerGold)
     {
+        spriteRenderer = GetComponent<SpriteRenderer>();
         this.enemySpawner = enemySpawner;
+        this.playerGold = playerGold;
 
         //최초 상태를 WeaponState.SearchTarget 으로 설정
         ChangeState(WeaponState.SearchTarget);
@@ -45,13 +48,13 @@ public class TowerWeapon : MonoBehaviour
         StartCoroutine(weaponState.ToString());
     }
 
-    //private void Update()
-    //{
+    private void Update()
+    {
     //    if (attackTarget != null)
     //    {
     //        RotateToTarget();
     //    }
-    //}
+    }
 
     //private void RotateToTarget()
     //{
@@ -77,7 +80,7 @@ public class TowerWeapon : MonoBehaviour
             {
                 float distance = Vector3.Distance(enemySpawner.EnemyList[i].transform.position, transform.position);
                 // 현재 검사중인 적과의 거리가 공격범위 내에 있고, 현재까지 검사한 적보다 거리가 가까우면
-                if (distance <= attackRange && distance <= closestDistSqr)
+                if (distance <= towerTemplate.weapon[level].range && distance <= closestDistSqr)
                 {
                     closestDistSqr = distance;
                     attackTarget = enemySpawner.EnemyList[i].transform;
@@ -106,7 +109,7 @@ public class TowerWeapon : MonoBehaviour
 
             // 2. target이 공격 범위 안에 있는지 검사 (공격 범위를 벗어나면 새로운 적 탐색)
             float distance = Vector3.Distance(attackTarget.position, transform.position);
-            if (distance > attackRange)                  // 적이 타워의 공격 범위를 벗어나도 SearchTarget으로 변경
+            if (distance > towerTemplate.weapon[level].range)                  // 적이 타워의 공격 범위를 벗어나도 SearchTarget으로 변경
             {
                 attackTarget = null;
                 ChangeState(WeaponState.SearchTarget);
@@ -114,7 +117,7 @@ public class TowerWeapon : MonoBehaviour
             }
 
             // 3. attackRate(공격주기) 시간만큼 대기
-            yield return new WaitForSeconds(attackRate);
+            yield return new WaitForSeconds(towerTemplate.weapon[level].rate);
 
             // 4. 공격 (발사체 생성)
             SpawnProjectile();
@@ -125,6 +128,24 @@ public class TowerWeapon : MonoBehaviour
     {
         GameObject clone = Instantiate(projectilePrefab, spawnPoint.position, Quaternion.identity);
         // 생성된 발사체에게 공격대상 (attackTarget) 정보 제공
-        clone.GetComponent<Projectile>().Setup(attackTarget, attackDamage);
+        clone.GetComponent<Projectile>().Setup(attackTarget, towerTemplate.weapon[level].damage);
+    }
+
+    public bool Upgrade()
+    {
+        // 타워 업그레이드에 필요한 골드가 충분한지 검사  골드가 부족하면 flase를 반환해 타워 업그레이드 실패 알림
+        if(playerGold.CurrentGold < towerTemplate.weapon[level+1].cost)
+        {
+            return false;
+        }
+        // 업글이 가능하면...
+        // 타워 레벨 증가
+        level++;
+        // 타워 외형 변경 (Sprite)
+        spriteRenderer.sprite = towerTemplate.weapon[level].sprite;
+        // 골드 차감
+        playerGold.CurrentGold -= towerTemplate.weapon[level].cost;
+
+        return true;
     }
 }
